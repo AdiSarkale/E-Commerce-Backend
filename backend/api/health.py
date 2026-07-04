e5c40078785032b9f299b749b3ad9818aef71eba
sAdityas <adissss009@gmail.com>
2026-07-04 16:21:55 +0530

    Added Health Check

--- backend/api/health.py
+++ backend/api/health.py
@@ -3,7 +3,7 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from redis.asyncio import Redis
 from db import get_db
-from utils.cache import CacheManager
+from core.cache import cached
 
 from fastapi import Depends
 
@@ -20,7 +20,7 @@ async def health():
 @router.get('/ready')
 async def readiness(
     db: AsyncSession = Depends(get_db),
-    redis: Redis = Depends(CacheManager)
+    redis: Redis = Depends(cached)
     ):
 
     db_status = 'connected'
